fae4803b695a39c6bd4257e7521b84cba7dc5db7
madesh66 <madeshw66@gmail.com>
2023-12-08 19:38:16 +0530

    hello program

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello {
+    public static void main(String[] args) {
+        LinkedList<Object>obj=new LinkedList<Object>();
+        Scanner mady=new Scanner(System.in);
+        for(int i=0;i<=5;i++) {
+            String name = mady.next();
+            if(name.endsWith("a")){
+            }}
+        System.out.println(obj);
+
+    }
+}
